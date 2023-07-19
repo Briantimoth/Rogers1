747e5f7150a071ef12d9b8c69d16890e53436c9a
Briantimoth <139984299+Briantimoth@users.noreply.github.com>
2023-07-19 10:06:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_so7eehu", "template_my8q3cn", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
